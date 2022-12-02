bbb9c557b04ca9955e9a72ca687a523846d4beb2
Ivan Roschin <ivan.roschin86@gmail.com>
2022-12-03 01:00:51 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,29 @@ import { ContactFilter } from './ContactFilter/ContactFilter';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 
+import {
+  Container,
+  PageTitle,
+  ContactsTitle,
+  TagLineBox,
+  ContactFormBox,
+} from './App.styled';
+
 export const App = () => {
   return (
-    <>
-      <ContactForm />
-      <ContactList />
-      <ContactFilter />
-    </>
+    <Container>
+      <PageTitle>Phonebook</PageTitle>
+      <TagLineBox>
+        <ContactFormBox>
+          <ContactsTitle>Contact form</ContactsTitle>
+          <ContactForm />
+        </ContactFormBox>
+        <div>
+          <ContactsTitle>Contacts List</ContactsTitle>
+          <ContactFilter />
+          <ContactList />
+        </div>
+      </TagLineBox>
+    </Container>
   );
 };
